f31b2cabf61f8fc702ec291cd8a23a016e3ef857
HMSachi <139098854+HMSachi@users.noreply.github.com>
2026-06-19 14:01:02 +0530

    interface

--- sample-React-project/src/components/HeaderComponent.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-const HeaderComponent = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-};
-
-export default HeaderComponent;
